edd93392ea85623607323f5e06875c202e0059f5
github username <travisemerymartin@gmail.com>
2019-10-14 20:20:05 -0400

    page templates

---
+++ src/pages/AboutPage.js
@@ -0,0 +1,13 @@
+import React, {} from 'react';
+
+import {
+} from '../Bin-Components.js';
+
+function temp() {
+    return (
+        <div className='temp'>
+        </div>
+    );
+};
+
+export default temp;
