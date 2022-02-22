6b237616d2ff2db3f165b4d6bb89b1a6ceea8849
Travis Martin <travisemerymartin@gmail.com>
2022-02-21 22:22:31 -0500

    Init commit

---
+++ src/pages/index.js
@@ -0,0 +1,14 @@
+import * as React from 'react';
+import "./index.css";
+
+const App = () => {
+	return (
+		<main>
+			<title>
+				Travis' Portfolio Site
+			</title>
+		</main>
+	);
+}
+
+export default App;
